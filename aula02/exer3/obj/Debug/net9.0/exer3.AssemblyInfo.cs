09e1873076731ccf23286102418336cb097cc7c8
caioaugustofm <caioaugusto.fm@gmail.com.br>
2026-06-16 09:37:31 -0300

    aula08

--- aula02/exer3/obj/Debug/net9.0/exer3.AssemblyInfo.cs
+++ aula02/exer3/obj/Debug/net9.0/exer3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exer3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8851c2c3722285c690f1ce0c3ea35c6fb2e53d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895f735926159aadc58870e39e4d3d382d37390f")]
 [assembly: System.Reflection.AssemblyProductAttribute("exer3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exer3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
