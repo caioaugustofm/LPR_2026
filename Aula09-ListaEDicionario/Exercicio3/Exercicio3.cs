e50b8aaf66e226c372597074e64a02f35afe1b5a
caioaugustofm <caioaugusto.fm@gmail.com>
2026-07-14 08:58:56 -0300

    LPR_commit

---
+++ Aula09-ListaEDicionario/Exercicio3/Exercicio3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        List<int> numeros = new List<int>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            numeros.Add(random.Next(0, 1001));
+        }
+
+        numeros.Sort();
+
+        Console.WriteLine("Ordem:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + "\n ");
+        }
+
+        numeros.RemoveAll(n => n % 2 == 0);
+
+        Console.WriteLine("Lista sem os números pares:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + "\n ");
+        }
+
+        Dictionary<int, int> contagem = new Dictionary<int, int>();
+
+        foreach (int numero in numeros)
+        {
+            if (contagem.ContainsKey(numero))
+                contagem[numero]++;
+            else
+                contagem[numero] = 1;
+        }
+
+        Console.WriteLine("Números repetidos:");
+
+        bool encontrou = false;
+
+        foreach (var item in contagem)
+        {
+            if (item.Value > 1)
+            {
+                Console.WriteLine($"{item.Key} aparece {item.Value} vezes");
+                encontrou = true;
+            }
+        }
+
+        if (!encontrou)
+        {
+            Console.WriteLine("Não existem números repetidos.");
+        }
+    }
+}
